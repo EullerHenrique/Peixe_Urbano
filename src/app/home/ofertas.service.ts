c425cf48f6e7200905f4caa5573c6cd42d9db57e
EullerHenrique <eullerhenrique@outlook.com>
2020-08-01 17:34:58 -0300

    feat: adds comments

--- src/app/home/ofertas.service.ts
+++ src/app/home/ofertas.service.ts
@@ -56,18 +56,6 @@ export class OfertasService{
 
     //Promise é um objeto usado para processamento assíncrono. Um Promise (de //"promessa") representa um valor que pode estar disponível agora, no //futuro ou nunca.
 
-        /*Um Promise representa um proxy para um valor que não é necessariamente conhecido quando a promessa é criada. Isso permite a associação de métodos de tratamento para eventos da ação assíncrona num caso eventual de sucesso ou de falha. Isto permite que métodos assíncronos retornem valores como métodos síncronos: ao invés do valor final, o método assíncrono retorna uma promessa ao valor em algum momento no futuro.
-
-        Um Promise está em um destes estados: 
-
-        pending (pendente): Estado inicial, que não foi realizada nem rejeitada.
-        fulfilled (realizada): sucesso na operação.
-        rejected (rejeitado):  falha na operação.
-        settled (estabelecida): Que foi realizada ou rejeitada.
-        
-        Uma promessa pendente pode se tornar realizada com um valor ou rejeitada por um motivo (erro). Quando um desses estados ocorre, o método then do Promise é chamado, e ele chama o método de tratamento associado ao estado (rejected ou resolved).
-        */
-
       return new Promise( 
       
       //O método Promise.resolve(value) retorna (para o método then) um objeto Promise que é resolvido com o valor passado.
