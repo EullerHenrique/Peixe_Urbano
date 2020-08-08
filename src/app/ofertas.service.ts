bd3137e83f83b743f6ac994366b6b9e4d3eb988b
EullerHenrique <eullerhenrique@outlook.com>
2020-08-08 19:53:09 -0300

    feat: adds getOfertasPorCategoria function

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,42 @@
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+import { Oferta } from "./shared/oferta.model";
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class OfertasService{
+
+  constructor(private http: Http){}
+
+
+  public getOfertas(): Promise<void|Oferta[]>{
+    return this
+    .http.get('http://localhost:3000/ofertas?destaque=true')
+    .toPromise()
+    .then(
+      
+      (ofertas: any) => {
+
+        return ofertas.json();
+     
+      }
+    )
+
+  }
+
+  public getOfertasPorCategoria(categoria: string): Promise<void|Oferta[]>{
+      return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .toPromise()
+      .then(
+      
+      (ofertas: any) => {
+
+        return ofertas.json();
+     
+      }
+    );
+  }
+
+    
+}
