88d70a946e13c588bbf22e340d74b19acd00af07
EullerHenrique <eullerhenrique@outlook.com>
2020-08-09 20:39:57 -0300

    refactor: refactor the footer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { RestaurantesComponent } from "./restaurantes/restaurantes.component";
 import { DiversaoComponent}  from "./diversao/diversao.component";
+import { OfertaComponent } from "./oferta/oferta.component";
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
