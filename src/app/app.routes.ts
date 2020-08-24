5fb672fabf10a5699962b0dd546525e1c7fddf22
EullerHenrique <eullerhenrique@outlook.com>
2020-08-23 21:09:20 -0300

    feat: adds comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { RestaurantesComponent } from "./restaurantes/restaurantes.component";
 import { DiversaoComponent}  from "./diversao/diversao.component";
 import { OfertaComponent } from "./oferta/oferta.component";
 import { componentFactoryName } from "@angular/compiler";
+import { ComoUsarComponent } from "./oferta/como-usar/como-usar.component";
+import { OndeFicaComponent } from "./oferta/onde-fica/onde-fica.component";
 
 export const ROUTES: Routes = [
   
@@ -11,7 +13,11 @@ export const ROUTES: Routes = [
   { path: 'restaurantes', component: RestaurantesComponent},
   { path: 'diversao', component: DiversaoComponent},
   { path: 'oferta', component: HomeComponent},
-  { path: 'oferta/:id', component: OfertaComponent}
+  { path: 'oferta/:id', component: OfertaComponent,
+   children:[
+    {path: 'como-usar', component: ComoUsarComponent},
+    {path: 'onde-fica', component: OndeFicaComponent}
+  ]}
 
 ]
 
