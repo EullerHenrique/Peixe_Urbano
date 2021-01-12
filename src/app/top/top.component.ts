34bb11e6e92bb548e1439f6334b9ded58ab69dcf
EullerHenrique <eullerhenrique@outlook.com>
2021-01-12 17:02:51 -0300

    feat: adds subject and switchMap

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
@@ -12,13 +14,39 @@ import { Oferta } from '../shared/oferta.model';
 export class TopComponent implements OnInit {
 
   private myObservable: Observable<Oferta[]>;
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private OfertasService: OfertasService) { }
 
   ngOnInit() {
+
+
+    this.myObservable = this.subjectPesquisa
+                       .switchMap((termoDaPesquisa: string) => {
+                          console.log('requisicao http para api');
+                          return this.OfertasService.pesquisaOfertas(termoDaPesquisa);
+                       });
+
+    
+    this.myObservable.subscribe((ofertas: Oferta[]) => 
+                                console.log(ofertas)
+                              );                  
+
+
   }
 
   public pesquisa(termoDaPesquisa: string): void{
+
+
+    // Observer (observador)
+    // Subject atuando na condição de observador
+
+    console.log('keyup caracter', termoDaPesquisa);
+    this.subjectPesquisa.next(termoDaPesquisa);
+
+
+
+    /*
 
     //Observable (Observavél)
     this.myObservable = this.OfertasService.pesquisaOfertas(termoDaPesquisa);
@@ -43,6 +71,8 @@ export class TopComponent implements OnInit {
     //Obs: Toda vez que o observavel adquirir um novo valor, o observador será notificado (o observador receberá o novo valor)
 
     this.myObservable.subscribe(myObserver)
+
+    */
 
   }
 
