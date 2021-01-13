7e1014cd2aa875bbdab73a3854d1ec75982673f6
EullerHenrique <eullerhenrique@outlook.com>
2021-01-12 21:13:19 -0300

    feat: adds distinctUntilChanged

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged'
 import 'rxjs/add/observable/of';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
@@ -25,6 +26,7 @@ export class TopComponent implements OnInit {
 
     this.ofertas = this.subjectPesquisa
                        .debounceTime(1000)
+                       .distinctUntilChanged()
                        .switchMap((termoDaPesquisa: string) => {
                           console.log('requisicao http para api');
 
