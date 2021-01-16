465c6320c2e99c2cdd46ac7199d5d86cb0dda27e
EullerHenrique <eullerhenrique@outlook.com>
2021-01-15 21:22:40 -0300

    feat: adds two-way-binding

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  private endereco: string = '';
+  private numero: number = null;
+  private complemento: string = '';
+  private formaDePagamento: string = '';
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(endereco: string): void{
+
+    this.endereco = endereco;
+
+  }
+
+  public atualizaNumero(numero: number): void{
+
+    this.numero = numero;
+
+  }
+
+  public atualizaComplemento(complemento: string): void{
+
+    this.complemento = complemento;
+
+  }
+  
+  public atualizaFormaDePagamento(formaDePagamento: string): void{
+
+    this.formaDePagamento = formaDePagamento;
+
   }
 
 }
