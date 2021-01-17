997e0431eac7874f2611ce027bac43cef7a23a1a
EullerHenrique <eullerhenrique@outlook.com>
2021-01-17 17:28:31 -0300

    feat: adds angular http

--- src/app/ordem-compra/ordem-compra.service.ts
+++ src/app/ordem-compra/ordem-compra.service.ts
@@ -1,6 +1,12 @@
 import { OrdemCompra } from './ordem-compra.model';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class OrdemCompraService{
+
+
+  constructor(private http: Http){}
 
   public efetivarCompra(ordemCompra:OrdemCompra):void{
 
