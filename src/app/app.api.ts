4f9abfcbe8fe5aee098bedebe828c9e591464e6a
EullerHenrique <eullerhenrique@outlook.com>
2021-01-21 14:39:34 -0300

    refactor: refactor app.api

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,4 +1,4 @@
-export const URL_API_OFERTAS = 'http://localhost:3000/ofertas';
-export const URL_API_COMO_USAR = 'http://localhost:3000/como-usar';
-export const URL_API_ONDE_FICA = 'http://localhost:3000/onde-fica';
-export const URL_API_PEDIDOS = 'http://localhost:3000/pedidos';
+export const URL_API_OFERTAS = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/ofertas';
+export const URL_API_COMO_USAR = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/como-usar';
+export const URL_API_ONDE_FICA = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/onde-fica';
+export const URL_API_PEDIDOS = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/pedidos';
