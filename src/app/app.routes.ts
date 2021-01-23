f414fa0d3a711655d9e0f65db994e62b17f76ffc
EullerHenrique <eullerhenrique@outlook.com>
2021-01-23 01:00:22 -0300

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,12 +12,10 @@ export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurantes', component: RestaurantesComponent},
   { path: 'diversao', component: DiversaoComponent},
-  { path: 'oferta', component: HomeComponent},
   { path: 'oferta/:id', component: OfertaComponent,
    children:
    
     [
-      {path: '', component: ComoUsarComponent},
       {path: 'como-usar', component: ComoUsarComponent},
       {path: 'onde-fica', component: OndeFicaComponent}
     ]
