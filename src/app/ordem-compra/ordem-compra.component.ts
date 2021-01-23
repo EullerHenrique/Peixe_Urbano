b6ea0c5d18d494689e0c767ad6ce8c5c263a5a39
EullerHenrique <eullerhenrique@outlook.com>
2021-01-23 16:32:36 -0300

    refactor

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { OrdemCompraService } from "./pedido.service";
+import { PedidoService } from "./pedido.service";
 import { Pedido } from "./pedido.model";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ItemCarrinho } from "./item-carrinho.model";
@@ -9,47 +9,46 @@ import { CarrinhoService } from "./carrinho.service";
   selector: "app-ordem-compra",
   templateUrl: "./ordem-compra.component.html",
   styleUrls: ["./ordem-compra.component.css"],
-  providers: [OrdemCompraService]
+  providers: [PedidoService]
 })
-
 export class OrdemCompraComponent implements OnInit {
-  
   public form: FormGroup = new FormGroup({
-    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(1)]),
-    'numero': new FormControl(null, [ Validators.required, Validators.nullValidator]),
-    'complemento': new FormControl(null),
-    'formaDePagamento': new FormControl(null, [ Validators.required ]),
+    endereco: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(1)
+    ]),
+    numero: new FormControl(null, [
+      Validators.required,
+      Validators.nullValidator
+    ]),
+    complemento: new FormControl(null),
+    formaDePagamento: new FormControl(null, [Validators.required])
   });
 
-  constructor(public ordemCompraService: OrdemCompraService, 
-              public carrinhoService: CarrinhoService) {}
+  constructor(
+    public PedidoService: PedidoService,
+    public carrinhoService: CarrinhoService
+  ) {}
 
   public idPedido: number;
   public itensCarrinho: ItemCarrinho[] = [];
 
   ngOnInit() {
-
     this.itensCarrinho = this.carrinhoService.exibirItens();
-  
   }
 
-  public confirmarCompra():void{
+  public confirmarCompra(): void {
+    let pedido: Pedido = new Pedido(
+      this.form.value.endereco,
+      this.form.value.numero,
+      this.form.value.complemento,
+      this.form.value.formaDePagamento,
+      this.carrinhoService.exibirItens()
+    );
 
-      let pedido: Pedido = new Pedido(
-                                this.form.value.endereco, 
-                                this.form.value.numero,
-                                this.form.value.complemento,
-                                this.form.value.formaDePagamento,
-                                this.carrinhoService.exibirItens()
-      );
-
-      this.ordemCompraService.efetivarCompra(pedido).subscribe( 
-      (idPedido: number) => {
-        this.idPedido = idPedido;
-        this.carrinhoService.limparCarrinho();
-      })
-  
+    this.PedidoService.efetivarCompra(pedido).subscribe((idPedido: number) => {
+      this.idPedido = idPedido;
+      this.carrinhoService.limparCarrinho();
+    });
   }
-
-
 }
--- src/app/ordem-compra/pedido.service.ts
+++ src/app/ordem-compra/pedido.service.ts
@@ -1,28 +1,28 @@
-import { Pedido } from './pedido.model';
-import { Http, RequestOptions, Headers, Response } from '@angular/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { URL_API_PEDIDOS } from '../app.api';
+import { Pedido } from "./pedido.model";
+import { Http, RequestOptions, Headers, Response } from "@angular/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { URL_API_PEDIDOS } from "../app.api";
 
 @Injectable()
-export class OrdemCompraService{
+export class PedidoService {
+  constructor(public http: Http) {}
 
-  constructor(public http: Http){}
-
-  public efetivarCompra(pedido:Pedido): Observable<number>{
-
+  public efetivarCompra(pedido: Pedido): Observable<number> {
     let headers: Headers = new Headers();
-    headers.append('Content-type','application/json'); //append = acrescentar
+    headers.append("Content-type", "application/json"); //append = acrescentar
 
-    return this.http.post(
-      `${URL_API_PEDIDOS}`,
-      JSON.stringify(pedido), // stringify = objeto literal -> string JSON
-      new RequestOptions( { 
-        headers: headers
-      })
-    )
-    .map((resposta: Response ) => resposta.json().id);
-
+    return this.http
+      .post(
+        `${URL_API_PEDIDOS}`,
+        JSON.stringify(pedido), // stringify = objeto literal -> string JSON
+        new RequestOptions({
+          headers: headers
+        })
+      )
+      .map((resposta: Response) => {
+        console.log(resposta.json());
+        return resposta.json().id;
+      });
   }
-
 }
