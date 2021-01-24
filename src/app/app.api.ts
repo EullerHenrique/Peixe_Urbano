bcfa6282ff825aa6fc04e369c512722af81c9c3b
EullerHenrique <48317736+EullerHenrique@users.noreply.github.com>
2021-01-23 21:47:13 -0300

    Delete src directory

--- src/app/app.api.ts
+++
@@ -1,4 +0,0 @@
-export const URL_API_OFERTAS = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/ofertas';
-export const URL_API_COMO_USAR = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/como-usar';
-export const URL_API_ONDE_FICA = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/onde-fica';
-export const URL_API_PEDIDOS = 'https://my-json-server.typicode.com/EullerHenrique/Peixe_Urbano/pedidos';
--- src/app/oferta/oferta.model.ts
+++
@@ -1,9 +0,0 @@
-export class Oferta{
-  public id: number;
-  public categoria: string;
-  public empresa: string;
-  public produto: string;
-  public valor: number;
-  public destaque: boolean;
-  public imagens: object;
-}
